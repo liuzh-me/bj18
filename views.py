8f8ecc05d762fe8e212cee8c100b4d3173216978
xwp <xwp_fullstack@163.com>
2024-01-08 21:54:38 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redict
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redict('/index')
